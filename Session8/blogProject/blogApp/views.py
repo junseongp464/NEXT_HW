c332b6f894a4eae0570eff146ff625d00ad03bc1
junseongp464 <junseongp464@naver.com>
2023-04-06 16:35:40 +0900

    NEXT

---
+++ Session8/blogProject/blogApp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def new(request):
+    if request.method == 'POST':
+        
+        print(request.POST)
+        
+        new_article = Article.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content'],
+            category = request.POST['category'],
+        )
+        return redirect('list')
+    
+    return render(request, 'new.html')
+
+
+def list(request):
+    startups = Article.objects.filter(category='startup')
+    startups_len = len(startups)
+
+    hobbies = Article.objects.filter(category='hobby')
+    hobbies_len = len(hobbies)
+
+    programmings = Article.objects.filter(category='programming')
+    programmings_len = len(programmings)
+
+    return render(request, 'list.html', {'startups_len': startups_len, 'hobbies_len': hobbies_len, 'programmings_len': programmings_len})
+    
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'detail.html', {'article':article})
+
+def category(request, category):
+    articles = Article.objects.filter(category=category)
+    return render(request, 'category.html', {'articles': articles, 'category': category})
