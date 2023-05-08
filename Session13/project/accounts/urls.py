89c4a3dff949ef0782ac78a32fcafc46dd0e5eed
junseongp464 <junseongp464@naver.com>
2023-05-08 17:30:05 +0900

    session14done

---
+++ Session13/project/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from accounts import views
+
+app_name = 'accounts'
+urlpatterns = [
+    # /accounts/
+    path('', views.home, name ='home'),
+]
+
---
+++ Session14/helloworld.js
@@ -0,0 +1,9 @@
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+for (let key in salaries.keys) {
+  salaries.Ann;
+}
