62736a76d0aa4b1149dc95357a34a30a0eacac07
junseongp464@naver.com <junseongp464@naver.com>
2023-05-11 19:41:15 +0900

    session15

---
+++ Session15/Session15_js복습/0-3navBarWithoutJs/main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".menu-button");
+
+const navbar = document.querySelector(".navbar");
+
+function toggleMenu() {
+  btn.classList.toggle("active");
+  navbar.classList.toggle("active");
+}
+
+btn.addEventListener("click", toggleMenu);
